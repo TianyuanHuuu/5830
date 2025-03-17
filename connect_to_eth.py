a2631cf7e9eae5d9a9c969c55a29a2d010ab780c
TianyuanHuuu <160672802+TianyuanHuuu@users.noreply.github.com>
2025-03-17 18:49:09 -0400

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,33 +11,30 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = "https://mainnet.infura.io/v3/8054c6b6305148d7be6d257adef7a4e6"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-	return w3
-
+    url = "https://mainnet.infura.io/v3/8054c6b6305148d7be6d257adef7a4e6"  # FILL THIS IN
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    return w3
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# TODO complete this method
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "wss://bsc-testnet.drpc.org"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    # Connect to Binance Smart Chain (BSC) testnet
+    url = "https://data-seed-prebsc-1-s1.binance.org:8545"  # HTTP URL
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-        contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
+    # Inject POA middleware for BSC chains
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
-	return w3, contract
+    # Create contract object
+    contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
 
+    return w3, contract
 
 if __name__ == "__main__":
-	connect_to_eth()
+    connect_to_eth()
